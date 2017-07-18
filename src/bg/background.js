a9e0f02d041b51f18421dc49f786b1c04613b35e
Austin Guevara <a.guevara@salesforce.com>
2017-07-17 21:22:03 -0400

    Minimize + try to fix

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,16 +1,17 @@
 // Variable to store the downloads URL
 var downloadsURL = 'chrome://downloads/';
 
-// Prevent repeated downloads opens
-// var justOpened = false;
-var isOpen = false;
-
 // Disable the bottom bar
 function disableShelf() {
     chrome.downloads.setShelfEnabled(false);
 }
 
-function openDownloads() {
+function openExisting(downloadTabs) {
+    var updateProperties = { "active": true };
+    chrome.tabs.update(downloadTabs[0].id, updateProperties, function (tab) { });
+}
+
+function newDownloadsTab() {
     // Create a new downloads tab
     chrome.tabs.create({
         url: downloadsURL,
@@ -18,51 +19,14 @@ function openDownloads() {
     });
 }
 
-// function checkDownloadsAlreadyOpen() {
-//     // If the tabs query returns a match, set isOpen to true.
-//     chrome.tabs.query({url: downloadsURL}, function(results) {
-//         if (results.length >= 1) {
-//             isOpen = true;
-//         } else {
-//             isOpen = false;
-//         }
-//     });
-// }
-
 chrome.downloads.onCreated.addListener(function(downloadItem) {
     chrome.downloads.onChanged.addListener(function(certainDownloadItem) {
+        console.log(certainDownloadItem);
         // Disable tab bar at the bottom of the window
         disableShelf();
 
-        // Update the already open status
-        // checkDownloadsAlreadyOpen();
-        // If the tabs query returns a match, set isOpen to true.
         chrome.tabs.query({url: downloadsURL}, function(results) {
-            if (results.length > 0) {
-                isOpen = true;
-                console.log('DOWNLOADS IS OPEN.');
-            } else {
-                isOpen = false;
-                console.log('DOWNLOADS NOT ALREADY OPEN.');
-            }
+            (results.length > 0) ? openExisting(results) : newDownloadsTab();
         });
-
-        // Open new downloads tab if one does not exist already
-        if (isOpen) {
-            console.log('TRY OPENING EXISTING');
-            chrome.tabs.query({ url: downloadsURL }, function (results) {
-                if (results.length > 0) {
-                    var updateProperties = { "active": true };
-                    chrome.tabs.update(results[0].id, updateProperties, function (tab) { });
-                }
-            });
-        } else {
-            openDownloads();
-            console.log('I OPENED A TAB.');
-        }
-
-        // Prevent repeated downloads opens
-        // justOpened = true;
-        // setTimeout(function(){ justOpened = false; }, 3000);
     });
 });
