1da7297ce46d56f37a02477b1353b46d0dbf0612
Austin Guevara <a.guevara@salesforce.com>
2017-09-26 21:05:40 -0400

    Improve file start observation

--- src/bg/background.js
+++ src/bg/background.js
@@ -2,16 +2,15 @@
 
 // Disable bottom downloads bar, always
 let disableShelf = () => chrome.downloads.setShelfEnabled(false);
-// chrome.runtime.onInstalled.addListener(disableShelf);
-// chrome.runtime.onStartup.addListener(disableShelf);
+chrome.runtime.onInstalled.addListener(disableShelf);
+chrome.runtime.onStartup.addListener(disableShelf);
 
-// Variable to store the downloads URL
 let downloadsURL = 'chrome://downloads/';
 
-// Existing Download IDs array
+var thisGuy = [];
+
 var existingDownloadIds = [];
 
-// Take a list of open downloads tabs and make the first one active
 function openExisting(downloadTabs) {
     var updateProperties = {
         "active": true
@@ -19,7 +18,6 @@ function openExisting(downloadTabs) {
     chrome.tabs.update(downloadTabs[0].id, updateProperties, function(tab) {});
 }
 
-// Create a new downloads tab
 function newDownloadsTab() {
     chrome.tabs.create({
         url: downloadsURL,
@@ -27,20 +25,16 @@ function newDownloadsTab() {
     });
 }
 
-// Fire event for every new download item that has changed
 chrome.downloads.onCreated.addListener(function(downloadItem) {
-    // Make sure the download item has a filename + filename is legit + is new
-    if (downloadItem.hasOwnProperty('filename') &&
-        downloadItem.filename.current != "" &&
-        existingDownloadIds.indexOf(downloadItem.id) < 0) {
-        // Add it to the existing downloads
-        existingDownloadIds.push(downloadItem.id);
-		disableShelf();
-        // Check if a downloads tab exists; if so, make active; if not, open new
-        chrome.tabs.query({
-            url: downloadsURL
-        }, function(results) {
-            (results.length > 0) ? openExisting(results): newDownloadsTab();
-        });
-    }
+	chrome.downloads.onChanged.addListener(function(changedDownloadItem) {
+		if (changedDownloadItem.hasOwnProperty('filename')
+			&& changedDownloadItem.id == downloadItem.id
+			&& changedDownloadItem.filename != downloadItem.filename) {
+				chrome.tabs.query({
+					url: downloadsURL
+				}, function(results) {
+					(results.length > 0) ? openExisting(results): newDownloadsTab();
+				});
+			}
+	});
 });
